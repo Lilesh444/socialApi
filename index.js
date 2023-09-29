aef41da4a9bb52dcb0b859516438a56d9ce807c9
Lilesh444 <spiku923@gmail.com>
2023-09-29 13:41:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get('/',(req,res)=>{
+  res.send("Hii this is my social api homePage , Bye!!")
+})
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
